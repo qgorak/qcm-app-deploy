1e495905a6edf9cc750ad7f6ec63f59f950ba963
jguillaumesio <62891175+jguillaumesio@users.noreply.github.com>
2020-12-07 16:44:06 +0100

    Image upload conditions + secondary menu

--- app/controllers/ImageController.php
+++ app/controllers/ImageController.php
@@ -14,23 +14,26 @@ class ImageController extends ControllerBase{
 	 * @post('add')
 	 */
 	public function add(){
+		$maxsize=2097152;
 		$availableType=["image/bmp","image/gif","image/jpeg","image/png"];
 		if(isset($_FILES['upload'])){
 			if(\in_array($_FILES['upload']['type'],$availableType)){
-				$tmp_name = $_FILES["upload"]["tmp_name"];
-				$ext=\end(explode("/",$_FILES["upload"]["type"]));
-				$user=USession::get('activeUser')['id'];
-				$uploads_dir = "upload/".$user."/";
-				$files = \glob($uploads_dir. "*");
-				if ($files){
-					$filecount =1+\count($files);
+				if(($_FILES['upload']['size'] < $maxsize) && ($_FILES["upload"]["size"] != 0)) {
+					$tmp_name = $_FILES["upload"]["tmp_name"];
+					$ext=\end(explode("/",$_FILES["upload"]["type"]));
+					$user=USession::get('activeUser')['id'];
+					$uploads_dir = "upload/".$user."/";
+					$files = \glob($uploads_dir. "*");
+					if ($files){
+						$filecount =1+\count($files);
+					}
+					if(!\is_dir($uploads_dir)){
+						\mkdir($uploads_dir);
+					}
+					$path=$uploads_dir.$filecount.".".$ext;
+					\move_uploaded_file($tmp_name,$path);
+					echo \json_encode(['url'=>$path]);
 				}
-				if(!\is_dir($uploads_dir)){
-					\mkdir($uploads_dir);
-				}
-				$path=$uploads_dir.$filecount.".".$ext;
-				\move_uploaded_file($tmp_name,$path);
-				echo \json_encode(['url'=>$path]);
 			}
 		}
 	}
--- public/assets/js/authModal.js
+++ public/assets/js/authModal.js
@@ -8,3 +8,12 @@ $('#registermodal')
   .modal('show')
 ;
 });
+$('#show-menu').click(function(){
+  if ( $('#second-menu').css('visibility') == 'hidden' )
+	$('#second-menu').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 200);
+  else
+    $("#second-menu").animate({opacity: 0}, 200, function(){
+    $("#second-menu").css("visibility","hidden");
+});
+});
+
