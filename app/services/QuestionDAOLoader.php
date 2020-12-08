0d6dc33520279e6a401226965271597198a4e9eb
qgorak <q.gorak@gmail.com>
2020-12-08 03:08:16 +0100

    sort by tag completed

--- app/services/QuestionDAOLoader.php
+++ app/services/QuestionDAOLoader.php
@@ -20,12 +20,11 @@ class QuestionDAOLoader {
 	        if (empty($res)) {
 	            $res = DAO::getManyToMany($tag, 'questions',true);
 	        }else{
-	            $questions = DAO::getManyToMany($tag, 'questions',true);
-                 
+	            $res = array_intersect($res, DAO::getManyToMany($tag, 'questions',true));
 	            }     
 	        }
 	        return $res;
-	    }
+	  }
 	
 
 	
