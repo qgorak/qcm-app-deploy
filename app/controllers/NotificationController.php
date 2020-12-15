886ff54bde85c08641226763e26e47e2843e0d5b
unknown <guillaume.jacopin@sts-sio-caen.info>
2020-12-15 19:14:49 +0100

    dynamic notifications

--- app/controllers/NotificationController.php
+++ app/controllers/NotificationController.php
@@ -34,6 +34,29 @@ class NotificationController extends ControllerBase{
      * @route('/','name'=>'notification')
      */
     public function index(){
+        $this->jquery->ajaxInterval('get',Router::path('notification'),30000,null,'#response',[
+            'hasLoader'=>false
+        ]);
+        $notifJson=$this->getNotifications();
+        USession::set('notifications',$notifJson);
+        $this->_index($this->jquery->renderView('NotificationController/display.html',['notifications'=>$notifJson],true));
+    }
+    
+    private function _index($response){
+        $this->jquery->renderView ( 'NotificationController/index.html', [
+            'notif' => $response
+        ] );
+    }
+    
+    private function refresh(){
+        $notifJson=$this->getNotifications();
+        if(USession::get('notifications')!=$notifJson){
+            USession::set('notifications',$this->getNotifications());
+        }
+        return $this->jquery->renderView('NotificationController/display.html',['notifications'=>$notifJson]);
+    }
+    
+    private function getNotifications(){
         date_default_timezone_set('Europe/Paris');
         $exam=$this->loader->getExamNotification();
         $groupDemand=$this->loader->getGroupNotification();
@@ -42,11 +65,10 @@ class NotificationController extends ControllerBase{
             array_push($notifJson,['id'=>$value->getId(),'title'=>'Examen en cours','timer'=>strtotime($value->getDated())-strtotime(date("Y-m-d H:i:s"))]);
         }
         foreach($groupDemand as $value){
-        	array_push($notifJson,['id'=>$value->getId(),'title'=>'Demande d\'accès au groupe','timer'=>null]);
+            array_push($notifJson,['id'=>$value->getId(),'title'=>'Demande d\'accès au groupe','timer'=>null]);
         }
-        $this->jquery->renderView('NotificationController/index.html',[
-            'notifications'=>$notifJson
-        ]);
+        return $notifJson;
     }
+    
 }
 
