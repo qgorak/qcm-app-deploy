45989042478ad132725f82387a87973d5b2f9db0
qgorak <q.gorak@gmail.com>
2020-12-21 17:30:34 +0100

    Account statistics

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -1,8 +1,12 @@
 <?php
 namespace controllers;
 
+use models\Exam;
 use models\Group;
+use models\Qcm;
 use models\Question;
+use models\Usergroup;
+use services\DAO\NotificationDAOLoader;
 use Ubiquity\orm\DAO;
 use Ubiquity\utils\http\URequest;
 use Ubiquity\utils\http\UResponse;
@@ -20,6 +24,20 @@ class DashboardController extends ControllerBase{
     use AclControllerTrait;
 
     private $uiService;
+    /**
+     *
+     * @autowired
+     * @var NotificationDAOLoader
+     */
+    private $loader;
+
+    /**
+     *
+     * @param \services\DAO\NotificationDAOLoader $loader
+     */
+    public function setLoader($loader) {
+        $this->loader = $loader;
+    }
 
     public function initialize() {
         parent::initialize ();
@@ -38,9 +56,15 @@ class DashboardController extends ControllerBase{
     public function index(){
         $countQuestion=DAO::count(Question::class,'idUser=?',[USession::get('activeUser')['id']]);
         $countGroup=DAO::count(Group::class,'idUser=?',[USession::get('activeUser')['id']]);
+        $countQcm=DAO::count(Qcm::class,'idUser=?',[USession::get('activeUser')['id']]);
+        $countExam=DAO::uCount(Exam::class,'qcm.idUser = ?',[USession::get('activeUser')['id']]);
+        $countUserInMyGroups=DAO::uCount(Usergroup::class,'group.idUser = ? and status=1',[USession::get('activeUser')['id']]);
         $this->jquery->renderView("DashboardController/index.html",[
             'nbQuestion'=>$countQuestion,
-            'nbGroup'=>$countGroup
+            'nbGroup'=>$countGroup,
+            'nbQcm'=>$countQcm,
+            'nbExam'=>$countExam,
+            'nbUsers'=>$countUserInMyGroups
         ]);
     }
 
