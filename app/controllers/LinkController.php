4e3c54aa339880f532c4bba0c5f1ee924c06df94
unknown <guillaume.jacopin@sts-sio-caen.info>
2021-01-10 00:25:30 +0100

    Join group by link

---
+++ app/controllers/LinkController.php
@@ -0,0 +1,64 @@
+<?php
+namespace controllers;
+
+use Ubiquity\controllers\Router;
+use Ubiquity\orm\DAO;
+use Ubiquity\security\acl\controllers\AclControllerTrait;
+use Ubiquity\translation\TranslatorManager;
+use Ubiquity\utils\http\USession;
+use services\DAO\GroupDAOLoader;
+use models\Usergroup;
+
+/**
+ * Controller LinkiController
+ */
+class LinkController extends ControllerBase{
+    use AclControllerTrait;
+    
+    /**
+     *
+     * @autowired
+     * @var GroupDAOLoader
+     */
+    private $loader;
+    
+    /**
+     *
+     * @param \services\DAO\GroupDAOLoader $loader
+     */
+    public function setLoader($loader) {
+        $this->loader = $loader;
+    }
+    
+    public function index(){}
+    
+    /**
+     * @allow('role'=>['@GUEST','@USER'])
+     * @get('link/{key}','name'=>'joinLink')
+     * @param string $key
+     */
+    public function joinByLink(string $key){
+        $user=USession::get('activeUser')['id'];
+        $group=$this->loader->getByKey($key);
+        if($group!=null){
+            if($user!=null){
+                if(!($this->loader->isInGroup($group->getId(), $user) || $this->loader->isCreator($group->getId(), $user) || $this->loader->alreadyDemand($group->getId(), $user))){
+                    $userGroup=new Usergroup();
+                    $userGroup->setIdGroup($group->getId());
+                    $userGroup->setIdUser($user);
+                    $userGroup->setStatus(0);
+                    DAO::save($userGroup);
+                    $this->jquery->semantic()->toast('body',['message'=>TranslatorManager::trans('joinSucceed',[],'main'),'class'=> 'success','position'=>'center top']);
+                }
+            }
+            else{
+                USession::set('redirect',Router::path('joinLink',[$key]));
+                $this->jquery->semantic()->toast('body',['message'=>'Veuillez vous connecter','class'=> 'success','position'=>'center top']);
+            }
+        }
+        else{
+            $this->jquery->semantic()->toast('body',['message'=>'Group not existing','class'=> 'warning','position'=>'center top']);
+        }
+        $this->jquery->renderView('MainController/index.html');
+    }
+}
