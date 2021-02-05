bf7ba302f006a280fe726020030e9fb31f213e5d
qgorak <58226291+qgorak@users.noreply.github.com>
2021-02-05 11:53:13 +0100

    Update BaseAuthController.php

--- app/controllers/BaseAuthController.php
+++ app/controllers/BaseAuthController.php
@@ -162,7 +162,7 @@ class BaseAuthController extends \Ubiquity\controllers\auth\AuthController{
                 $instance->setPassword(URequest::post('password'));
             }
             $confirmedId=$instance->getFirstname().\uniqid();
-            $instance->setConfirmed($confirmedId);
+            $instance->setConfirmed(1);
             $colors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"];
             $color = $colors[\array_rand($colors)];
             $instance->setAvatar($color);
